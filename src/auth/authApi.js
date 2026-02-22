da5643d44877926854ee3521556a9c46a5cd450e
Utkarsh Pawar <utkarshpawar2332@gmail.com>
2026-02-22 13:53:18 +0530

    added github actions

--- src/auth/authApi.js
+++ src/auth/authApi.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8765",
+  baseURL: window.env?.API_BASE_URL || import.meta.env.VITE_API_BASE_URL || "http://localhost:30765",
   timeout: 30_000,
 });
 
